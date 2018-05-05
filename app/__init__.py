e5773287107ad9585a016b6fb49f034c48bc114c
Akeam Williams <akeam.williams@mymona.uwi.edu>
2018-05-05 04:16:12 +0000

    update

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ app.config['SECRET_KEY'] = "372ijma/.D/V,A,KK,ASK,1mejlADK\S][SP;SLKK2I231JHRMWG
 #app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://group18:akeam1804@localhost/project2"
 app.config['SQLALCHEMY_DATABASE_URI'] ="postgresql://feuowhhypgdbxv:199f7305d3d8c5a7548dc3052f86e459c3766813cac235db2c62f710543403cc@ec2-23-23-180-121.compute-1.amazonaws.com:5432/dfp9t8rfb53oso"
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True  # added just to suppress a warning
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= True  # added just to suppress a warning
 app.config['UPLOAD_FOLDER']= "./app/static/uploads/" # using a config value
 
 db = SQLAlchemy(app)
